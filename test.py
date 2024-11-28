83281ac3c9165db62d61eb0dde820a5650b69742
gyojincompany <luckyred@gmail.com>
2024-11-28 20:49:25 +0900

    기간 설정 함수 추가

---
+++ test.py
@@ -0,0 +1,8 @@
+year = 2020
+month = 12
+
+202012
+
+yyyymm = f"{year}" + f"{month:0>2}"
+
+print(yyyymm)
--- tourApi.py
+++ tourApi.py
@@ -1,5 +1,6 @@
 import urllib.request
 import datetime
+import json
 
 # import requests
 #
@@ -25,7 +26,7 @@ def getRequestUrl(url):
 
 def getTourismStatsItem(yyyymm, nat_cd, ed_cd):
     baseUrl = "http://openapi.tour.go.kr/openapi/service/EdrcntTourismStatsService/getEdrcntTourismStatsList"
-    parameters = "?&serviceKey="
+    parameters = "?_type=json&serviceKey="
     parameters = parameters + f"&YM={yyyymm}"
     parameters = parameters + f"&NAT_CD={nat_cd}"
     parameters = parameters + f"&ED_CD={ed_cd}"
@@ -33,3 +34,23 @@ def getTourismStatsItem(yyyymm, nat_cd, ed_cd):
     url = baseUrl + parameters
 
     responseDecode = getRequestUrl(url)
+    
+    if responseDecode == None: # 참이면 에러 발생!
+        return None
+    else:  # 정상적인 응답 성공
+        return json.loads(responseDecode)
+
+
+# print(getTourismStatsItem("202403", "112", "E"))
+# 
+# num = getTourismStatsItem("202403", "112", "E")
+# num2 = num["response"]["body"]["items"]["item"]["num"]
+# print(num2)
+
+# 시작 년월과 종료 년월을 인수로 넣으면 해당 기간 동안의 출입국 관광객 수를 가져오는 함수
+def getTourismStatsService(nat_cd, ed_cd, nStartYear, nEndYear):  # 국가코드, 출입국여부, 시작년도, 종료년도
+    for year in range(int(nStartYear), int(nEndYear)+1): # 2020, 2023
+        for month in range(1, 13):  # 1~12월
+            yyyymm = f"{year}"+f"{month:0>2}"
+            getTourismStatsItem(yyyymm, nat_cd, ed_cd)
+
